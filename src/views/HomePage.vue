a16411a3b4d828035e3a235d2ad3b8e0e283b26b
Théo Laperrouse <theo.laperrouse@energiency.com>
2023-10-14 16:41:39 +0200

    feat: Add FontAwesome

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,24 @@
 <template>
     <div class="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center">
-        <h1 class="text-2xl mb-5">Template</h1>
+        <h1 class="text-2xl mb-5">
+            Template Vue / Vite :
+            <a
+                href="https://github.com/TheoLaperrouse/TemplateViteVue3"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="text-white"
+            >
+                <FontAwesomeIcon :icon="['fab', 'github']" /> GitHub
+            </a>
+        </h1>
     </div>
 </template>
+<script>
+import FontAwesomeIcon from '../../fontawesome';
+
+export default {
+    components: {
+        FontAwesomeIcon,
+    },
+};
+</script>
